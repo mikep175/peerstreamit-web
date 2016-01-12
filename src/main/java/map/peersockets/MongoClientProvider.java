3f4727e65f9bac3240dbcb26fedac78e52cf9275
mikep175 <mikeycp1@gmail.com>
2016-01-11 22:19:32 -0500

    f

---
+++ src/main/java/map/peersockets/MongoClientProvider.java
@@ -0,0 +1,40 @@
+package map.peersockets;
+
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.ConcurrencyManagement;
+import javax.ejb.ConcurrencyManagementType;
+import javax.ejb.Lock;
+import javax.ejb.LockType;
+import javax.ejb.Singleton;
+ 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+ 
+@Singleton
+@ConcurrencyManagement(ConcurrencyManagementType.CONTAINER)
+public class MongoClientProvider {
+		
+	private MongoClient mongoClient = null;
+		
+	@Lock(LockType.READ)
+	public MongoClient getMongoClient(){	
+		return mongoClient;
+	}
+	
+	@PostConstruct
+	public void init() {
+		
+		MongoCredential credential = MongoCredential.createCredential(System.getenv("OPENSHIFT_MONGODB_DB_USERNAME"),
+				System.getenv("peersockets"),
+				System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD").toCharArray());
+		
+		mongoClient = new MongoClient(new ServerAddress(
+				System.getenv("OPENSHIFT_MONGODB_DB_HOST"), Integer.valueOf(System.getenv("OPENSHIFT_MONGODB_DB_PORT"))),
+				Arrays.asList(credential));//System.getenv("OPENSHIFT_MONGODB_DB_HOST"), Integer.valueOf(System.getenv("OPENSHIFT_MONGODB_DB_PORT")), credential);
+	}
+		
+}
--- src/main/java/map/peersockets/rest/Login.java
+++ src/main/java/map/peersockets/rest/Login.java
@@ -1,5 +1,6 @@
 package map.peersockets.rest;
 
+import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -7,17 +8,45 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.bson.Document;
+
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import map.peersockets.MongoClientProvider;
+
 @RequestScoped
 @Path("/login")
 @Produces({ "application/xml", "application/json" })
 @Consumes({ "application/xml", "application/json" })
 public class Login {
 
+	@EJB
+	MongoClientProvider mongoClientProvider;
+	
 	@POST
     @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
     public boolean login(LoginRequest req) {
         
+		MongoClient mClient = mongoClientProvider.getMongoClient();
+		
+		MongoDatabase db = mClient.getDatabase("peersockets");
+		
+		db.createCollection("users");
+		
+		MongoCollection<Document> collection = db.getCollection("users");
+
+		// insert a document
+		Document document = new Document("userId", 1);
+		
+		document.put("mp", "mp");
+		
+		collection.insertOne(document);
+		
 		return true;
 		
 		
