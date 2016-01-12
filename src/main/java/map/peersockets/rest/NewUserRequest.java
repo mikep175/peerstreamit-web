61940b1be99623f5661a287e470b8c98252d6a8e
mikep175 <mikeycp1@gmail.com>
2016-01-11 22:46:24 -0500

    g

---
+++ src/main/java/map/peersockets/rest/NewUserRequest.java
@@ -0,0 +1,35 @@
+package map.peersockets.rest;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement
+public class NewUserRequest {
+
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		
+		return this.username;
+		
+	}
+	
+	public String getPassword() {
+		
+		return this.password;
+		
+	}
+	
+	public void setUsername(String username) {
+		
+		this.username = username;
+		
+	}
+	
+	public void setPassword(String password) {
+		
+		this.password = password;
+		
+	}
+}
