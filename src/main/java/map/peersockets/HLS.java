a735aa45627197712893c2a41660ec3ce1b7f7d5
Mike Peterson <mikeycp1@gmail.com>
2016-07-20 08:24:33 -0400

    hls

---
+++ src/main/java/map/peersockets/HLS.java
@@ -0,0 +1,31 @@
+package map.peersockets;
+
+import java.io.File;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/HLS")
+@Consumes({ "application/json" })
+@Produces({ "application/json" })
+public class HLS {
+ 
+    @GET
+    @Path("/test")
+    public String test() {
+    	return "test";
+    }
+    
+//    @GET
+//    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+//    public Response getFile() {
+//      File file = new File();
+//      return Response.ok(file, MediaType.APPLICATION_OCTET_STREAM)
+//          .header("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"" ) //optional
+//          .build();
+//    }
+}
