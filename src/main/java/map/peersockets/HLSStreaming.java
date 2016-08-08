a6807ef3e066678c561d2b295284bc0cd315f714
Mike Peterson <mikeycp1@gmail.com>
2016-08-08 17:22:03 -0400

    sdfsdfdsf

--- src/main/java/map/peersockets/HLSStreaming.java
+++ src/main/java/map/peersockets/HLSStreaming.java
@@ -32,11 +32,11 @@ public class HLSStreaming {
 		Logger.getLogger(BinaryWebSocketServer.class.getName()).log(Level.INFO, "playlist.m3u8 : " + hlsId);
 		  
 		StringBuilder ret = new StringBuilder("#EXTM3U\r\n" +
-				//"#EXT-X-PLAYLIST-TYPE:VOD\r\n" +
+				"#EXT-X-PLAYLIST-TYPE:VOD\r\n" +
 				"#EXT-X-TARGETDURATION:4\r\n" +
-				//"#EXT-X-VERSION:4\r\n" +
-				"#EXT-X-MEDIA-SEQUENCE:0");// + //\r\n
-				//"#EXT-X-MAP:URI=moov.mp4\r\n");
+				"#EXT-X-VERSION:4\r\n" +
+				"#EXT-X-MEDIA-SEQUENCE:0" +// + //\r\n
+				"#EXT-X-MAP:URI=moov.mp4\r\n");
 		
 		 DecimalFormat decimalFormat=new DecimalFormat("#");
 		 
